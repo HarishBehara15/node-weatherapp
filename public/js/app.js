9ba5a64dec0ba82a313550167719453f5a8b2ba5
harish behara <harish.behara@yahoo.com>
2021-08-23 18:09:51 +0530

    Remove Concole.log in app.js

--- public/js/app.js
+++ public/js/app.js
@@ -1,27 +1,3 @@
-
-// console.log('This is client side java script');
-
-
-// const weatherForm = document.querySelector('form')
-// const search = document.querySelector('input')
-// const messageOne=document.querySelector('#message-1')
-// messageOne.textContent= 'sample'
-
-// weatherForm.addEventListener('submit',(e) => {
-//     e.preventDefault()
-
-//     const location = search.value
-
-//     fetch('http://localhost:5000/weather?location='+location).then((response) => {
-//         response.json().then((data) => {
-//             console.log(data)
-//         })
-//     })
-// }) 
-
-
-console.log('Client side javascript file is loaded!')
-
 const weatherForm = document.querySelector('form')
 const search = document.querySelector('input')
 const messageOne = document.querySelector('#message-1')
@@ -41,7 +17,7 @@ weatherForm.addEventListener('submit', (e) => {
                 messageOne.textContent = data[0].error
             } else {
                 messageOne.textContent = data.location
-                messageTwo.textContent = data[0].forecast
+                messageTwo.textContent = data[0].forecast 
             }
 
         })
